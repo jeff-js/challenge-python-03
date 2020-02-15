2002766f41ad0f4f0d28eafa9200d2d6b8d22bcb
Jefferson Aguilar <jefferson@alunices.com>
2020-02-15 10:50:31 -0500

    Solución de la busqueda del texto oculto

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
-
+    encoded_text = ""
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        encoded_text = f.read()
+    character_find = re.findall("[a-z]",encoded_text)
+    hidden_word = ''.join(character_find)
+    print(hidden_word)
+    #[ESTE ES EL MENSAJE OCULTO] -> christianvanderhenst
 
 if __name__ == '__main__':
     run()
